04f04f512f9e8c6499a671b6abdcc72006c6f9c9
daniel-walters <waltersdaniel96@gmail.com>
2021-05-18 13:04:33 +0800

    reads player names from file

--- src/game.rb
+++ src/game.rb
@@ -8,11 +8,14 @@ require "egd"
 require "json"
 
 class Game
+    attr_reader :player_white, :player_black
     attr_accessor :cur_board, :turn, :player_to_move
-    def initialize
+    def initialize(white, black)
         @cur_board = Board.new
         @turn = 1
         @player_to_move = "w"
+        @player_white = white
+        @player_black = black
     end
 
     def change_turn
--- src/index.rb
+++ src/index.rb
@@ -2,12 +2,14 @@ require_relative "game"
 require_relative "file_reader"
 require_relative "display"
 
-game = Game.new
 reader = FileReader.new("bobby.pgn")
+white = reader.data["game_tags"]["White"]
+black = reader.data["game_tags"]["Black"]
+game = Game.new(white, black)
 draw_board(game.cur_board)
 move_info = reader.get_move("#{game.turn}#{game.player_to_move}")
 game.cur_board.move_piece(move_info)
 sleep(1)
 draw_board(game.cur_board)
-
+puts "#{game.player_white} vs. #{game.player_black}"
 
